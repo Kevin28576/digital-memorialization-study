1998bcff00fd9648095269b903604a4ef1dec7ad
Kevin <89570292+Kevin28576@users.noreply.github.com>
2025-06-05 14:34:02 +0800

    upload

--- src/components/VotingSection.tsx
+++ src/components/VotingSection.tsx
@@ -1,4 +1,3 @@
-// src/components/VotingSection.tsx
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
